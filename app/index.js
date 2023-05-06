790a90715eaff542c8b8517ffa18e460b14817fd
david <daviidgt03@gmail.com>
2023-05-07 00:22:49 +0200

    Refactor functions and add test to them

--- app/index.js
+++ app/index.js
@@ -1,32 +1,68 @@
-const newLength = (array) => {
+const length = (array) => {
   let position = 0;
 
-  for (const element in array) {
-    position++;
+  for (const i in array) {
+    if (array) position++;
   }
 
   return position;
 };
 
-const newPush = (array, ...newElement) => {
-  const openedArray = [...array];
+const push = (array, ...newElement) => {
+  const newThings = [...newElement];
+  for (let i = 0; i < newThings.length; i++) {
+    array[array.length] = newThings[i];
+  }
 
-  const newPushedArray = [...openedArray, ...newElement];
-
-  return newLength(newPushedArray);
+  return array.length;
 };
 
-const newPushButGivesArrayInstead = (array, ...newElement) => {
-  const openedArray = [...array];
-
-  return [...openedArray, ...newElement];
+const filter = (array, argument) => {
+  for (let i = 0; i < i.length; i++) {
+    for (let j = 0; j[i] < j.length; j++) {
+      if (array[i][j] <= argument) {
+        return array;
+      }
+    }
+  }
 };
 
-const newFind = (array, argument) => {
-  const length = newLength(array);
+console.log(filter([3, 7], 5));
 
-  for (let index = 0; index < length; index++) {
-    if (array[index] === argument) {
+const pop = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  const lastElement = array[array.length - 1];
+  array.length -= 1;
+  return lastElement;
+};
+
+const shift = (array) => {
+  const cloneArray = [...array];
+  array.length = 0;
+  for (let i = 1; i < cloneArray; i++) {
+    array[array[i]] = cloneArray[i];
+  }
+
+  return cloneArray[0];
+};
+
+const unshift = (array, ...newElement) => {
+  const mergedArray = [...newElement, ...array];
+  array.length = 0;
+  for (let i = 0; i < mergedArray.length; i++) {
+    array[i] = mergedArray[i];
+  }
+
+  return array.length;
+};
+
+const some = (array, callBack) => {
+  for (let i = 1; i < array.length; i++) {
+    const element = array[i];
+    if (callBack(element)) {
       return true;
     }
   }
@@ -34,4 +70,4 @@ const newFind = (array, argument) => {
   return false;
 };
 
-export default { newLength, newPush, newPushButGivesArrayInstead, newFind };
+export { length, push, pop, shift, unshift, some };
--- app/index.test.js
+++ app/index.test.js
@@ -1,51 +1,95 @@
-import index from "./index.js";
+import { length, push, pop, shift, unshift, some } from "./index.js";
 
-describe("Given the function newLength", () => {
+describe("Given the function length", () => {
   describe("When given the array of 1, 2 ,3", () => {
     test("Then it should return 3", () => {
       const array = [1, 2, 3];
 
       const expectedResult = 3;
-      const resultReturned = index.newLength(array);
+      const resultReturned = length(array);
+
+      expect(resultReturned).toBe(expectedResult);
+    });
+  });
+  describe("When given an empty array  ", () => {
+    test("Then it should return 0", () => {
+      const array = [];
+
+      const expectedResult = 0;
+      const resultReturned = length(array);
 
       expect(resultReturned).toBe(expectedResult);
     });
   });
 
-  describe("When given the array of 5, 10 , 15, 20, 25", () => {
-    test("Then it should return 5", () => {
-      const array = [5, 10, 15, 20, 25];
+  describe("Given the function push", () => {
+    describe("When given the array of 1,2,3 and a 4 ", () => {
+      test("Then it should return and array with 4", () => {
+        const array = [1, 2, 3];
+        const value = 4;
 
-      const expectedResult = 5;
-      const resultReturned = index.newLength(array);
+        const expectedResult = 4;
+        const resultReturned = push(array, value);
+
+        expect(resultReturned).toBe(expectedResult);
+      });
+    });
+  });
+  describe("Given the function pop", () => {
+    describe("When given the array of 1,2,3 ", () => {
+      test("Then it should delete the 3 of the array  ", () => {
+        const array = [1, 2, 3];
+
+        const expectedResult = 3;
+        const resultReturned = pop(array);
+
+        expect(resultReturned).toBe(expectedResult);
+      });
+    });
+  });
+
+  describe("Given the function shift", () => {
+    describe("When given the array of 1,2,3 ", () => {
+      test("Then it should delete the 1 of the array  ", () => {
+        const array = [1, 2, 3];
+
+        const expectedResult = 1;
+        const resultReturned = shift(array);
+
+        expect(resultReturned).toBe(expectedResult);
+      });
+    });
+  });
+  describe("When given an empty array", () => {
+    test("Then it should return undefined", () => {
+      const array = [];
+
+      const expectedResult = undefined;
+      const resultReturned = shift(array);
 
       expect(resultReturned).toBe(expectedResult);
     });
   });
-});
+  describe("Given the function unshift", () => {
+    describe("When given the array of 1,2,3  and another one with 4 ,5", () => {
+      test("Then it should return a 4 ", () => {
+        const array = [1, 2, 3];
+        const newNumbers = 4;
 
-describe("Given the function newPush", () => {
-  describe("When given the array 1, 2 and given the values of 3, 4, 5", () => {
-    test("Then it should return 5", () => {
-      const array = [1, 2];
-      const valueA = 3;
-      const valueB = 4;
-      const valueC = 5;
+        const expectedResult = 4;
+        const resultReturned = unshift(array, newNumbers);
 
-      const expectedResult = 5;
-      const resultReturned = index.newPush(array, valueA, valueB, valueC);
-
-      expect(resultReturned).toBe(expectedResult);
+        expect(resultReturned).toBe(expectedResult);
+      });
     });
   });
+  describe("When given a 4 to an empty array ", () => {
+    test("Then it should return an array with 1 ", () => {
+      const array = [];
+      const newNumbers = 4;
 
-  describe("When given the array 'hola', 'joseba' and given the array of 'como' 'es' 'estas?'", () => {
-    test("Then it should return 5", () => {
-      const array = ["hola", "joseba"];
-      const valueA = ["como", "es", "estas?"];
-
-      const expectedResult = 3;
-      const resultReturned = index.newPush(array, valueA);
+      const expectedResult = 1;
+      const resultReturned = unshift(array, newNumbers);
 
       expect(resultReturned).toBe(expectedResult);
     });
